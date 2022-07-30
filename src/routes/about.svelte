6856bce8563d810848e7b2af07dc37ad1a9a8b25
taco <tylerchall16@gmail.com>
2022-07-29 21:42:42 -0500

    commit 1

---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<title>About the sveltekit Pokedex</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">about</h1>
+<p>This is a really cool sveltekit project that uses the PokeAPI.</p>
---
+++ tailwind.config.cjs
@@ -0,0 +1,4 @@
+module.exports = {
+	node: 'jit',
+	purge: ['./src/**/*.svelte']
+};
